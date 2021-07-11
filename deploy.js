295f0857a26e0e22fa37820f267b60a56c9aeca9
Phillip Rose <rosecodingaccount@protonmail.com>
2021-07-11 15:48:01 -0700

    update deploy.js file

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.238.145.119',
+      host: '3.235.55.13',
       username: 'ubuntu',
       privateKey: 'hhs-key.pem'
     })
